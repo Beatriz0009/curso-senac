21bd864a2f467bc52ad8f6ad1d2595dc37ff61e9
Beatriz0009 <biaavettorazzi@gmail.com>
2024-11-26 12:55:34 +0000

    gravar arquivo

---
+++ python/aula2.py
@@ -0,0 +1,40 @@
+# faturamento = 1200 # Tipo: int -> número inteiro. 
+# custo = 750.32 # Tipo: float -> número com casas decimais.
+# lucro = faturamento - custo 
+
+# margemLucro = lucro/faturamento
+
+# print("Faturamento foi de ", faturamento)
+# print("O custo foi de ", custo)
+# print(O lucro foi de", lucro)
+# print("A margem de lucro foi de ", margemLucro)
+
+# menssagem = "O faturamento da loja foi de tanto"
+# print(type(mensagem))
+# print("A variavel faturamento é do tipo ", type(faturamento))
+# email ="emailqualquer@gmail.com" # Tipo string -> texto str.
+# teve_lucro = True  # Variavel tipo boolean.
+# print(typee(teve_lucro))
+
+#Mod -> resto da divisao.
+print(10 % 3)
+tempo_contrato = 170
+tempo_anos = tempo_contrato / 12
+tempo_meses = tempo_contrato % 12
+
+print("tempo em anos: ", int(tempo_anos), " anos e ", tempo_meses, " meses.")
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = "Ana Beatriz Vettorazzi Cardoso", 15
+print(nome)
+print(idade) 
+
+nome2, idade2 = "", 0
+print(type(nome2))
+print(type(idade2)) 
+print("a variavel nome2 é do tipo ", type(nome2))
+print("A variavel idade2 é do tipo", type(idade2)) 
+print(type(n3))
+print(type(estado))
+print(type(1+2j))
